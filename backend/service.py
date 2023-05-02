851976f868a76eb1b12a2386956d31c7120e8a2b
Robin Dong <robin.k.dong@gmail.com>
2023-05-02 20:41:58 +1000

    Add ChatGPT API and testing wav

--- backend/service.py
+++ backend/service.py
@@ -2,8 +2,16 @@ import time
 import openai
 
 from flask import Flask, request
+from embeddings import SentenceEmbeddings
+from storage import FaissStorage
+
+INDEX_FILE = ".birds.faiss"
+MAX_QUERIES = 5
 
 app = Flask(__name__)
+fs = FaissStorage()
+fs.load(INDEX_FILE)
+se = SentenceEmbeddings()
 
 @app.route("/", methods=["POST", "GET"])
 def whisper():
@@ -14,13 +22,24 @@ def whisper():
         file.save(filename)
         begin = time.time()
         audio = open(filename, "rb")
-        result = openai.Audio.transcribe("whisper-1", audio)
+        query = openai.Audio.transcribe("whisper-1", audio)["text"]
+        print("Q:", query)
+        em = se.generate_embedding(query)
+        ans = "".join(fs.get(em, MAX_QUERIES))
+        prompt = f"You are a friendly assistant. Please read below text and answer the questions clearly, directly and shortly:\n\n{ans}\n\n"
+        resp = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": query},
+            ]
+        )
+        res = resp["choices"][0]["message"]["content"]
+        print("A:", res)
         print("time:", time.time() - begin)
     else:
-        print(request)
-        result = {"text": "Hello world!"}
-    print(result["text"])
-    return result["text"]
+        res = "Hello world"
+    return res
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
