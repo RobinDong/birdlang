b2101bb9880556d6a3175f9461e6feeca246a88e
Robin Dong <robin.k.dong@gmail.com>
2023-05-11 19:48:15 +1000

    Use separate knowledge base for Eng/Chinese

--- backend/service.py
+++ backend/service.py
@@ -6,12 +6,55 @@ from embeddings import SentenceEmbeddings
 from storage import FaissStorage
 
 INDEX_FILE = ".birds.faiss"
+ZH_INDEX_FILE = ".birds_zh.faiss"
 MAX_QUERIES = 5
 
 app = Flask(__name__)
 fs = FaissStorage()
 fs.load(INDEX_FILE)
+zh_fs = FaissStorage()
+zh_fs.load(ZH_INDEX_FILE)
 se = SentenceEmbeddings()
+
+def translate(prompt):
+    resp = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0301",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.1,
+        max_tokens=200,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    return resp["choices"][0]["message"]["content"]
+
+def chat_translate(eng_text, refs):
+    prompt = f"Learning these knowledge about bird name translation:\n\n{refs}\n\nNow translate below english text in the triple backticks shortly to Chinese sentence without double quotes: \n\n```{eng_text}```\n\nAnswer:"
+    resp = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo-0301",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.1,
+        max_tokens=200,
+        top_p=1.0,
+    )
+    return resp["choices"][0]["message"]["content"]
+
+def openai_ask(refs, query):
+    prompt = f"Read and understand below text in the triple backticks and answer the question by following rules: 1. Answer the question politely and completely. 2. If the text does not provide information for the question, just answer the question by using knowledge from Wikipedia or eBird.org. 3. Don't say \"not mentioned\" in the answer. \n\n```{refs}```\n\nQuestion: {query}\nAnswer: "
+    resp = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo-0301",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.1,
+        max_tokens=200,
+        top_p=1.0,
+    )
+    return resp["choices"][0]["message"]["content"]
 
 @app.route("/", methods=["POST", "GET"])
 def whisper():
@@ -28,36 +71,20 @@ def whisper():
             print(request.form)
             init_query = request.form["question"]
         print("I:", init_query)
-        resp = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Translate this into English:\n\n{init_query}\n\n",
-            temperature=0.3,
-            max_tokens=100,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
-        )
-        query = resp["choices"][0]["text"]
+        query = translate(f"Translate this into English:\n\n{init_query}\n\n")
         print("Q:", query.strip())
         em = se.generate_embedding(query)
-        ans = "\n\n".join(fs.get(em, MAX_QUERIES))
-        print("Related: ", ans)
-        prompt = f"Read and understand below text in the triple backticks and answer the question by following rules: \n1. Answer the question shortly. \n2. Answer the question by English language at first, and then answer the question by Chinese language. \n3. If the text does not provide information for the question, just answer the question by using knowledge from web (include Wikipedia and eBird.org).' \n\n```{ans}```\n\nQuestion: {query}"
-        resp = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo-0301",
-            messages=[
-                {"role": "user", "content": prompt},
-            ],
-            temperature=0.1,
-            max_tokens=200,
-            top_p=1.0,
-        )
-        res = resp["choices"][0]["message"]["content"]
+        refs = "\n\n".join(fs.get(em, MAX_QUERIES) + zh_fs.get(em, MAX_QUERIES))
+        print("Related: ", refs)
+        res = openai_ask(refs, query)
         print("A:", res)
+        refs = "\n\n".join(zh_fs.get(em, MAX_QUERIES))
+        zh_res = chat_translate(res, refs)
+        print("ZH_A:", zh_res)
         print("time:", time.time() - begin)
     else:
         res = "Hello world"
-    return f"{res}"
+    return f"{res}\n{zh_res}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
--- backend/test.sh
+++ backend/test.sh
@@ -1,3 +1,19 @@
 curl -X POST \
-  -i -F "audio_data=@/home/robin/Downloads/question_largest_bird.wav" \
+  -i -F "question='Purple Sandpiper的中文名是什么？" \
+  http://192.168.0.10:8000
+
+curl -X POST \
+  -i -F "question='世界上最小的鸟是什么？" \
+  http://192.168.0.10:8000
+
+curl -X POST \
+  -i -F "question='请问什么鸟的翼展最大？" \
+  http://192.168.0.10:8000
+
+curl -X POST \
+  -i -F "question='请问世界上最大的鸟是什么？" \
+  http://192.168.0.10:8000
+
+curl -X POST \
+  -i -F "question='请问世界上最大的会飞的鸟是什么？" \
   http://192.168.0.10:8000
